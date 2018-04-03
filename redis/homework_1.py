a631a70f63255e421f9ed8cfe8f848b3e8a017ae
David Arredondo <dca2123@columbia.edu>
2018-04-03 18:34:03 -0400

    squashed the bug

--- redis/homework_1.py
+++ redis/homework_1.py
@@ -1,15 +1,14 @@
-# make sure to "pip install requests"
+# written in python3. May need to pip intall requests, although probably not
 import requests
 import ast
 
-s =  requests.get("https//api.nasa.gov//planetary/apod?api_key=2SpOXDx3rOunTdmn9GQS0ShJSfoC8p7pLJ0LgMDK") #&date=2017-09-26")
-print(s)
-print("\n")
+#http request
+s =  requests.get("https://api.nasa.gov/planetary/apod?api_key=2SpOXDx3rOunTdmn9GQS0ShJSfoC8p7pLJ0LgMDK&date=2017-09-26")
+#convert the string to a python dictionary
+d = ast.literal_eval(s.text)
 
-d = ast.literal_eval(s)
-print(d)
-
+#get the url from the string
 url = d["url"]
-print("\n")
+#print the url
 print(url)
 
